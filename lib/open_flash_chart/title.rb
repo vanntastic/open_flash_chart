32bd6db711cc4e68c6708d97a2db6670bf212c89
vann <vann@innerfusion.net>
2009-08-20 11:14:01 -0500

    update readme and add some notes to the title object

--- lib/open_flash_chart/title.rb
+++ lib/open_flash_chart/title.rb
@@ -1,6 +1,20 @@
 module OpenFlashChart
 
   class Title < Base
+    # Examples :
+    # 
+    #   title = Title.new "My Title"
+    # 
+    # you can also pass in a block to use the set_style method to change the style of the title:
+    # 
+    #   title = Title.new "My Title" { |t| t.set_style "{font-size:10px}"}
+    # 
+    # OR : 
+    # 
+    #   title = Title.new "My Title"
+    #   title.set_style "{font-size:10px}"
+    # 
+    
     def initialize(text='', args = {})
       super args
       @text = text      
